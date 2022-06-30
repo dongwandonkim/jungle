13c7e2b177ebe9d7bec89620c809838347576191
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-06-30 01:11:29 +0000

    rework dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USERNAME"],
+                               password: ENV["HTTP_BASIC_PASSWORD"],
+                               if: -> { ENV["HTTP_BASIC_PASSWORD"].present? }
+
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
   end
 end
